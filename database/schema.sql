a15ef97b36fdea473112153f56dbb92f6ab55644
Evan Hong <e.hong1424@gmail.com>
2023-04-17 21:23:35 +0000

    updated DB import code

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,30 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."Users" (
+	"UserID" serial NOT NULL,
+	"Name" serial NOT NULL,
+	"Password" TEXT NOT NULL,
+	"Role" TEXT NOT NULL,
+	"Latitude" DECIMAL NOT NULL,
+	"Longitude" DECIMAL NOT NULL,
+	CONSTRAINT "Users_pk" PRIMARY KEY ("UserID")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."Locations" (
+	"LocationID" serial NOT NULL,
+	"Country" serial NOT NULL,
+	"Region" TEXT NOT NULL,
+	"Longitude" DECIMAL NOT NULL,
+	"Latitude" DECIMAL NOT NULL,
+	"Name" TEXT NOT NULL,
+	"Description" TEXT NOT NULL,
+	CONSTRAINT "Locations_pk" PRIMARY KEY ("LocationID")
+) WITH (
+  OIDS=FALSE
+);
