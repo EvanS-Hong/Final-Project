bc42b74965068001522cc22cf1e624b8cc7c1014
Evan Hong <e.hong1424@gmail.com>
2023-04-19 05:11:41 +0000

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  SignUpForm from './Signupform.js';
 
 function App() {
   return (
     <>
       <img className='button'  src="https://cdn-icons-png.flaticon.com/512/56/56763.png" alt="Menu button" />
+        <SignUpForm />
     </>
   );
 }
