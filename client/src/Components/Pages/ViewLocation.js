a458af2df8618c48f129afe92a6f9f8a77450170
Evan Hong <e.hong1424@gmail.com>
2023-04-24 02:35:35 +0000

    WIP

--- client/src/Components/Pages/ViewLocation.js
+++ client/src/Components/Pages/ViewLocation.js
@@ -1,46 +1,43 @@
-import React from 'react'
+import { useEffect, useState, React } from 'react'
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import './ViewLocation.css';
-import Geocode from "react-geocode";
+import usePlacesAutoComplete, {getGeocode, getLatLng } from 'use-places-autocomplete';
 
 
 export default function ViewLocation() {
-  // const [latitude, setLatitude] = useState();
-  // const [longitude, setLongitude] = useState();
-  // const [location, setlocation] = useState(undefined);
+  const [latitude, setLatitude] = useState(undefined);
+  const [longitude, setLongitude] = useState();
+  const [location, setLocation] = useState(undefined);
 
-
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   if (location === undefined) {
-  //     setLatitude();
-  //     setLongitute();
-  // }
-
-
-
-  Geocode.setApiKey("");
+  useEffect(() => {
+    async function displayLocation(address) {
+      try {
+        const results = await getGeocode({ address });
+        const { lat, lng } = await getLatLng(results[0]);
+        setLocation({ lat, lng});
+        setLatitude(lat);
+        setLongitude(lng);
+      } catch(err) {
+        console.error('Error!:', err);
+      }
+    }
+  }, []);
 
     const containerStyle = {
       width: '70vw',
       height: '70vh'
     };
 
-    const center = {
-      lat: -3.745,
-      lng: -38.523
-    };
-
     const { isLoaded } = useJsApiLoader({
       id: 'google-map-script',
-      googleMapsApiKey: ""
+      googleMapsApiKey: "AIzaSyAA9yzP9GJ7dLzvbAD5ybpzCm1ImSy4TqA"
     })
 
     const [map, setMap] = React.useState(null)
 
     const onLoad = React.useCallback(function callback(map) {
       // This is just an example of getting and using the map instance!!! don't just blindly copy!
-      const bounds = new window.google.maps.LatLngBounds(center);
+      const bounds = new window.google.maps.LatLngBounds(location);
       map.fitBounds(bounds);
 
       setMap(map)
@@ -52,28 +49,17 @@ export default function ViewLocation() {
 
     return isLoaded ? (
       <>
-        {/* <form onSubmit={handleSubmit}>
-          <h1> {text} </h1>
-          <h1> {statusMessage} </h1>
-          <label>
-            username
-            <input
-              name="userName"
-              type="userName"
-              value={userName}
-              onChange={e => setUserName(e.target.value)} />
-          </label>
-        </form> */}
+        <PlacesAutoComplete onSelect={(latLng, address) =>  setLocation(latLng)} />
         <div className="map">
           <GoogleMap
             mapContainerStyle={containerStyle}
-            center={center}
+            center={location}
             zoom={10}
             onLoad={onLoad}
             onUnmount={onUnmount}
           >
             <Marker
-              position={center}
+              position={location}
             />
             { /* Child components, such as markers, info windows, etc. */}
             <></>
@@ -81,4 +67,38 @@ export default function ViewLocation() {
         </div>
       </>
     ) : <></>
+}
+
+
+  const PlacesAutoComplete = ({ onSelect }) => {
+    const {
+      ready,
+      value,
+      setValue,
+      suggestions: { status, data },
+      clearSuggestions
+    } = usePlacesAutoComplete();
+
+
+  const handleSelect = async address => {
+    setValue(address, false);
+    clearSuggestions();
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      onSelect({ lat, lng }, address);
+    } catch (err) {
+      console.error('Error:', err);
+    }
   }
+
+   return (
+      <form onSelect={handleSelect}>
+        <input
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        disabled={!ready}
+        placeholder="placehere" />
+      </form>
+  );
+}
